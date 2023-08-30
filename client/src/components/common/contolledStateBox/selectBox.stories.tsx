ca54781f0832a0abe7fefc2623a26a26c2674b28
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-08-30 22:43:23 +0900

    🌈feat : selectBox 추가

--- client/src/components/common/contolledStateBox/selectBox.stories.tsx
+++ client/src/components/common/contolledStateBox/selectBox.stories.tsx
@@ -1,25 +1,25 @@
 import styled from 'styled-components';
 
-import {StoryFn} from '@storybook/react';
+import { StoryFn } from '@storybook/react';
 import SelectBox from '@components/common/contolledStateBox/selectBox';
 
 export default {
-    title: 'common/selectBox',
-    component: SelectBox,
-    tags: ['autodocs'],
-    decorators: [
-        (Story: StoryFn) => {
-            return (
-                <Container>
-                    <Story/>
-                </Container>
-            );
-        },
-    ],
-    argTypes: {
-        labelName: {control: 'text'},
-        state: {control: 'text'},
+  title: 'common/selectBox',
+  component: SelectBox,
+  tags: ['autodocs'],
+  decorators: [
+    (Story: StoryFn) => {
+      return (
+        <Container>
+          <Story />
+        </Container>
+      );
     },
+  ],
+  argTypes: {
+    labelName: { control: 'text' },
+    state: { control: 'text' },
+  },
 };
 
 const Container = styled.div`
@@ -27,46 +27,46 @@ const Container = styled.div`
 `;
 
 export const ListSortControl = {
-    args: {
-        labelName: '순차정렬',
-        state: '',
-        minWidth: 120,
-        MenuItems: [
-            {
-                id: 1,
-                title: '오름차순',
-                value: '오름차순',
-            },
-            {
-                id: 2,
-                title: '내림차순',
-                value: '내림차순',
-            },
-        ],
-    },
+  args: {
+    labelName: '순차정렬',
+    state: '',
+    minWidth: 120,
+    MenuItems: [
+      {
+        id: 1,
+        title: '오름차순',
+        value: '오름차순',
+      },
+      {
+        id: 2,
+        title: '내림차순',
+        value: '내림차순',
+      },
+    ],
+  },
 };
 
 export const ListLengthControl = {
-    args: {
-        labelName: '목록갯수설정',
-        state: 20,
-        minWidth: 150,
-        MenuItems: [
-            {
-                id: 1,
-                title: '10',
-                value: '10',
-            },
-            {
-                id: 2,
-                title: '20',
-                value: '20',
-            },
-            {
-                id: 3,
-                title: '30',
-                value: '30',
-            },
-        ],
-    },
+  args: {
+    labelName: '목록갯수설정',
+    state: 20,
+    minWidth: 150,
+    MenuItems: [
+      {
+        id: 1,
+        title: '10',
+        value: '10',
+      },
+      {
+        id: 2,
+        title: '20',
+        value: '20',
+      },
+      {
+        id: 3,
+        title: '30',
+        value: '30',
+      },
+    ],
+  },
 };
