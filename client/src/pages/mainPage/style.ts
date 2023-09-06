8e7b50e026e2c1edd9d0dbae93d18186b793fcf6
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-09-06 20:09:15 +0900

    💄style : 코드 포맷팅 누락

--- client/src/pages/mainPage/style.ts
+++ client/src/pages/mainPage/style.ts
@@ -2,16 +2,16 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
   position: relative;
-  z-index: ${({theme}) => theme.Z_INDEX['LEVEL-1']};
+  z-index: ${({ theme }) => theme.Z_INDEX['LEVEL-1']};
   width: 100%;
   max-width: 1856px;
   min-height: 520px;
   border-radius: 40px;
-  background: ${({theme}) => theme.COLOR['gray-5']};
+  background: ${({ theme }) => theme.COLOR['gray-5']};
 `;
 
 const S = {
-    Wrapper,
+  Wrapper,
 };
 
 export default S;
