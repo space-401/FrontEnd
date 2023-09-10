f39b6130176affc76160c523853003f4bb4d027c
ooherin <555ohr@naver.com>
2023-09-11 01:19:14 +0900

    🌈feat : 이미지 편집 라이브러리 변경

--- client/src/hooks/common/useSelectPhoto.ts
+++ client/src/hooks/common/useSelectPhoto.ts
@@ -1,17 +1,17 @@
-import { useRef, useState } from 'react';
+import { useState } from 'react';
 import { ChangeEvent } from 'react';
 
 const useSelectPhoto = () => {
-  const fileInputRef = useRef<HTMLInputElement | null>(null);
+  // const fileInputRef = useRef<HTMLInputElement | null>(null);
   //이미지 파일을 저장하는 곳
-  const [imgFile, setImgFile] = useState('');
+  const [image, setImage] = useState('');
 
-  // const handleBoxClick = () => {
-  //   // 클릭 이벤트가 발생했을 때 파일 입력 요소를 클릭
-  //   if (fileInputRef.current) {
-  //     fileInputRef.current.click();
-  //   }
-  // };
+  const handleInputClick = () => {
+    // 클릭 이벤트가 발생했을 때 파일 입력 요소를 클릭
+    // if (fileInputRef.current) {
+    //   fileInputRef.current.click();
+    // }
+  };
 
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     const selectedFile = e.target?.files?.[0];
@@ -20,7 +20,7 @@ const useSelectPhoto = () => {
       if (selectedFile) {
         reader.onloadend = () => {
           if (typeof reader.result === 'string') {
-            setImgFile(reader.result);
+            setImage(reader.result);
           }
         };
         reader.readAsDataURL(selectedFile);
@@ -29,11 +29,10 @@ const useSelectPhoto = () => {
   };
 
   return {
-    // handleBoxClick,
+    handleInputClick,
     handleFileChange,
-    fileInputRef,
-    imgFile,
-    setImgFile,
+    image,
+    setImage,
   };
 };
 
