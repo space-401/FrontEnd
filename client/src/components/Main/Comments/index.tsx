e438d0020678d9555161c23eb5ed0ea52c2d64b9
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-09-16 15:26:45 +0900

    🌈feat : mainPage state 한개로 수정

--- client/src/components/Main/Comments/index.tsx
+++ client/src/components/Main/Comments/index.tsx
@@ -1,61 +1,59 @@
 import S from '@components/Main/Comments/style';
-import React, { SetStateAction, useEffect, useState } from 'react';
-import { CommentType } from '@type/space.type';
+import {useEffect, useState} from 'react';
+import {CommentType} from '@type/space.type';
 import commentMock from '@mocks/data/DetailPage/comment.mock';
 import OneComment from '@components/Main/Comments/OneComment/OneComment';
 
 type DetailCommentType = {
-  isOpen: boolean;
-  isReply: { open: boolean; id: number | undefined } | undefined;
-  setIsReply: React.Dispatch<
-    SetStateAction<{ open: boolean; id: number | undefined } | undefined>
-  >;
+    isOpen: boolean;
+    isReply: { open: boolean; id: number | undefined } | undefined;
+    setIsReply: (newReply: { open: boolean; id: number | undefined } | undefined) => void;
 };
 
 const DetailComments = (props: DetailCommentType) => {
-  const { setIsReply, isReply, isOpen } = props;
-  const [list, setList] = useState<CommentType[]>([]);
+    const {setIsReply, isReply, isOpen} = props;
+    const [list, setList] = useState<CommentType[]>([]);
 
-  useEffect(() => {
-    setList(commentMock);
-  }, []);
+    useEffect(() => {
+        setList(commentMock);
+    }, []);
 
-  const ReplyClose = () => {
-    setIsReply({ open: false, id: undefined });
-  };
+    const ReplyClose = () => {
+        setIsReply({open: false, id: undefined});
+    };
 
-  const ReplyOpen = (id: number) => {
-    if (isReply?.open) {
-      ReplyClose();
-    }
-    setIsReply({ open: true, id: id });
-  };
+    const ReplyOpen = (id: number) => {
+        if (isReply?.open) {
+            ReplyClose();
+        }
+        setIsReply({open: true, id: id});
+    };
 
-  return (
-    <>
-      <S.Wrapper>
-        <S.CommentList isOpen={isOpen}>
-          {list.map((item) => (
-            <OneComment
-              key={item.id}
-              id={item.id}
-              refId={item.refId}
-              writer={item.writer}
-              content={item.content}
-              createDate={item.createDate}
-              updateDate={item.updateDate}
-              isRef={item.isRef}
-              isMyComment={item.isMyComment}
-              replyMember={item.replyMember}
-              ReplyOpen={ReplyOpen}
-              ReplyClose={ReplyClose}
-              isReply={isReply}
-            />
-          ))}
-        </S.CommentList>
-      </S.Wrapper>
-    </>
-  );
+    return (
+        <>
+            <S.Wrapper>
+                <S.CommentList isOpen={isOpen}>
+                    {list.map((item) => (
+                        <OneComment
+                            key={item.id}
+                            id={item.id}
+                            refId={item.refId}
+                            writer={item.writer}
+                            content={item.content}
+                            createDate={item.createDate}
+                            updateDate={item.updateDate}
+                            isRef={item.isRef}
+                            isMyComment={item.isMyComment}
+                            replyMember={item.replyMember}
+                            ReplyOpen={ReplyOpen}
+                            ReplyClose={ReplyClose}
+                            isReply={isReply}
+                        />
+                    ))}
+                </S.CommentList>
+            </S.Wrapper>
+        </>
+    );
 };
 
 export default DetailComments;
--- client/src/pages/main/MainBody/MainBody.tsx
+++ client/src/pages/main/MainBody/MainBody.tsx
@@ -2,83 +2,98 @@ import DefaultImage from '@assets/svg/KKIRI.svg';
 import FlipCard from '@components/Main/FlipCard/FlipCard';
 import SpaceInfoBack from '@components/Main/FlipCard/SpaceInfoBack/SpaceInfoBack';
 import S from './style';
-import { useState } from 'react';
-import { MainBodyPropType, selectType } from '@type/main.type';
+import {useEffect, useState} from 'react';
+import {MainBodyPropType, selectType} from '@type/main.type';
 import SelectBox from '@components/Main/SelectBox';
 import MainSearchBox from '@components/Main/SearchBox';
 import KaKaoMap from '@components/Main/PostMap';
-import { useDetailModalStore } from '@store/modal';
+import {useDetailModalStore} from '@store/modal';
 
 const MainBody = (props: MainBodyPropType) => {
-  const { postList, userList, tagList, selectState } = props;
-  const [_1, setUserState] = useState<selectType[]>([]);
-  const [_2, setTagState] = useState<selectType[]>([]);
-  const [_3, setSearchState] = useState<string>('');
-  const modalOpen = useDetailModalStore((state) => state.ModalOpen);
-  console.log(_1);
-  console.log(_2);
-  console.log(_3);
-  return (
-    <S.Wrapper>
-      <S.FilterGroup>
-        <SelectBox
-          setState={setUserState}
-          menuWidth={316}
-          menuHeight={20 + 32 * Math.ceil(userList.length / 2)}
-          BoxWidth={168}
-          labelName={'사용자'}
-          ListItem={userList}
-        />
-        <SelectBox
-          placeHolder={'태그명을 검색해주세요.'}
-          setState={setTagState}
-          menuWidth={192}
-          menuHeight={49 * Math.floor(tagList.length / 2)}
-          BoxWidth={168}
-          labelName={'태그'}
-          ListItem={tagList}
-        />
-        <MainSearchBox placeholder={'제목'} setState={setSearchState} />
-      </S.FilterGroup>
-      {!selectState ? (
-        postList.length === 0 ? (
-          <S.UndefinedList>
-            <S.UndefinedDefaultImage img_url={DefaultImage} />
-            <S.UndefinedPostText>
-              첫번째 게시글을 등록해 주세요
-            </S.UndefinedPostText>
-            <S.UndefinedShareText>게시글 공유하기</S.UndefinedShareText>
-          </S.UndefinedList>
-        ) : (
-          <S.PostList>
-            {postList.map((item) => (
-              <FlipCard
-                onClick={modalOpen}
-                key={item.post_id}
-                img_url={item.main_img_url}
-                hoverCard={
-                  <SpaceInfoBack
-                    position={item.position}
-                    users={item.users}
-                    post_id={item.post_id}
-                    post_title={item.place_title}
-                    main_img_url={item.main_img_url}
-                    place_title={item.place_title}
-                    post_updated_at={item.post_updated_at}
-                    post_created_at={item.post_created_at}
-                    place_tag={item.place_tag}
-                    key={item.post_id}
-                  />
-                }
-              />
-            ))}
-          </S.PostList>
-        )
-      ) : (
-        <KaKaoMap postList={postList} />
-      )}
-    </S.Wrapper>
-  );
+    const {postList, userList, tagList, selectState} = props;
+    const [state, setState] = useState<{ user: selectType[], tag: selectType[], search: string }>({
+        user: [],
+        tag: [],
+        search: ""
+    })
+    const modalOpen = useDetailModalStore((state) => state.ModalOpen);
+
+    const setUserState = (newUserState: selectType[]) => {
+        setState(prev => ({...prev, user: newUserState}))
+    }
+
+    const setTagState = (newTagState: selectType[]) => {
+        setState(prev => ({...prev, tag: newTagState}))
+    }
+
+    const setSearchState = (newSearch: string) => {
+        setState(prev => ({...prev, search: newSearch}))
+    }
+    useEffect(() => {
+        console.log(state)
+    }, [state])
+
+    return (
+        <S.Wrapper>
+            <S.FilterGroup>
+                <SelectBox
+                    setState={setUserState}
+                    menuWidth={316}
+                    menuHeight={20 + 32 * Math.ceil(userList.length / 2)}
+                    BoxWidth={168}
+                    labelName={'사용자'}
+                    ListItem={userList}
+                />
+                <SelectBox
+                    placeHolder={'태그명을 검색해주세요.'}
+                    setState={setTagState}
+                    menuWidth={192}
+                    menuHeight={49 * Math.floor(tagList.length / 2)}
+                    BoxWidth={168}
+                    labelName={'태그'}
+                    ListItem={tagList}
+                />
+                <MainSearchBox placeholder={'제목'} setState={setSearchState}/>
+            </S.FilterGroup>
+            {!selectState ? (
+                postList.length === 0 ? (
+                    <S.UndefinedList>
+                        <S.UndefinedDefaultImage img_url={DefaultImage}/>
+                        <S.UndefinedPostText>
+                            첫번째 게시글을 등록해 주세요
+                        </S.UndefinedPostText>
+                        <S.UndefinedShareText>게시글 공유하기</S.UndefinedShareText>
+                    </S.UndefinedList>
+                ) : (
+                    <S.PostList>
+                        {postList.map((item) => (
+                            <FlipCard
+                                onClick={modalOpen}
+                                key={item.post_id}
+                                img_url={item.main_img_url}
+                                hoverCard={
+                                    <SpaceInfoBack
+                                        position={item.position}
+                                        users={item.users}
+                                        post_id={item.post_id}
+                                        post_title={item.place_title}
+                                        main_img_url={item.main_img_url}
+                                        place_title={item.place_title}
+                                        post_updated_at={item.post_updated_at}
+                                        post_created_at={item.post_created_at}
+                                        place_tag={item.place_tag}
+                                        key={item.post_id}
+                                    />
+                                }
+                            />
+                        ))}
+                    </S.PostList>
+                )
+            ) : (
+                <KaKaoMap postList={postList}/>
+            )}
+        </S.Wrapper>
+    );
 };
 
 export default MainBody;
