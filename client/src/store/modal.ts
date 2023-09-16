fe45f8933b7c8b096b80f8270a19eb535020fd28
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-09-16 15:25:51 +0900

    🌈feat : alertModal 추가

--- client/src/store/modal.ts
+++ client/src/store/modal.ts
@@ -1,31 +1,46 @@
-import { create } from 'zustand';
+import {create} from 'zustand';
+import {AlertModalType} from "@type/modal.type";
 
 type ModalType = {
-  isOpen: boolean;
-  ModalOpen: () => void;
-  ModalClose: () => void;
+    isOpen: boolean;
+    ModalOpen: () => void;
+    ModalClose: () => void;
 };
 
 type ModeType = {
-  isMode: boolean;
-  ModeOn: () => void;
-  ModeClose: () => void;
+    isMode: boolean;
+    ModeOn: () => void;
+    ModeClose: () => void;
 };
 
 export const useDetailModalStore = create<ModalType>((setState) => ({
-  isOpen: false,
-  ModalOpen: () => setState(() => ({ isOpen: true })),
-  ModalClose: () => setState(() => ({ isOpen: false })),
+    isOpen: false,
+    ModalOpen: () => setState(() => ({isOpen: true})),
+    ModalClose: () => setState(() => ({isOpen: false})),
+}));
+
+export const useAlertModalStore = create<ModalType & {
+    info: AlertModalType | undefined;
+    setInfo: (newInfo: AlertModalType) => void;
+}>((setState) => ({
+    isOpen: false,
+    info: undefined,
+    setInfo: (newInfo: AlertModalType) => setState(() => ({
+        isOpen: false,
+        info: newInfo
+    })),
+    ModalOpen: () => setState((prev) => ({...prev, isOpen: true})),
+    ModalClose: () => setState(() => ({isOpen: false, info: undefined})),
 }));
 
 export const usePhotoModalStore = create<ModalType>((setState) => ({
-  isOpen: false,
-  ModalOpen: () => setState(() => ({ isOpen: true })),
-  ModalClose: () => setState(() => ({ isOpen: false })),
+    isOpen: false,
+    ModalOpen: () => setState(() => ({isOpen: true})),
+    ModalClose: () => setState(() => ({isOpen: false})),
 }));
 
 export const useEditModeStore = create<ModeType>((setState) => ({
-  isMode: false,
-  ModeOn: () => setState(() => ({ isMode: true })),
-  ModeClose: () => setState(() => ({ isMode: false })),
+    isMode: false,
+    ModeOn: () => setState(() => ({isMode: true})),
+    ModeClose: () => setState(() => ({isMode: false})),
 }));
