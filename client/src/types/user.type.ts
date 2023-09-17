2af5a1dcb5315b05ef83fd4a61cb4fa2cf9cf905
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-09-17 23:43:00 +0900

    🌈feat : 상세페이지 진행중, 헤더 레이아웃 변경

--- client/src/types/user.type.ts
+++ client/src/types/user.type.ts
@@ -1,5 +1,6 @@
 export type UserType = {
-  user_id: number;
-  user_name: string;
-  user_profile_img?: string;
+    user_id: number;
+    user_name: string;
+    user_profile_img?: string;
+    user_email?: string;
 };
