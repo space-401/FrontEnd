1f99ef2201ef59aa7de23b240f56ec09bd0f17fe
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-09-17 21:58:17 +0900

    💄style : 코드 포맷팅 누락

--- client/src/components/Main/Comments/OneComment/OneComment.tsx
+++ client/src/components/Main/Comments/OneComment/OneComment.tsx
@@ -161,7 +161,7 @@ const OneComment = (props: OneCommentType) => {
             <S.ReplyTextBox>
               <TextareaAutosize
                 value={state.reply}
-                style={{ paddingRight: '50px'}}
+                style={{ paddingRight: '50px' }}
                 minRows={2}
                 maxRows={4}
                 onChange={handleChange}
