8dbbf0096c59ee751bc1d9331c8cef67e75b9427
jason-lee-webdev <jason.lee.webdev@gmail.com>
2023-09-19 16:47:37 +0900

    🚀chore : Spring Boot kkiri 프로젝트 생성
     - Spring Boot Web 프로젝트를 생성했다.
     - .gitignore 파일에 Spring Boot 프로젝트와 IntelliJ에 해당하는 내용 추가하고 기존 내용 중 겹치는 내용은 삭제했다.
    
     close #69

---
+++ server/src/main/java/com/app/kkiri/KkiriApplication.java
@@ -0,0 +1,13 @@
+package com.app.kkiri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkiriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkiriApplication.class, args);
+    }
+
+}
