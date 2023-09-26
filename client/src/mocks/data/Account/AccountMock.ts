e084280ffdb794f4cded27a8614803cf9ed75318
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-09-27 01:46:27 +0900

    🌈feat : 마이페이지 임시완성

--- client/src/mocks/data/Account/AccountMock.ts
+++
@@ -1,7 +0,0 @@
-import { AccountDataType } from '@pages/MyPage/MyBody/components/AccountManagement';
-
-export const AccountMock: AccountDataType = {
-  loginState: '카카오톡',
-  name: '김은주',
-  email: 'qwer@kakao.com',
-};
