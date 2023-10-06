4ede8bdf1870f8cd2ab90c3be32ed74ebad8c139
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-10-06 22:32:11 +0900

    🌈feat : 메인 페이지 퍼블리싱 수정

--- client/src/components/Main/PostMap/components/style.ts
+++ client/src/components/Main/PostMap/components/style.ts
@@ -1,32 +1,18 @@
 import styled from 'styled-components';
+import { omitText } from '@styles/common';
 
 const Wrapper = styled.div`
   background: ${({ theme }) => theme.COLOR['gray-7']};
-  padding: 24px 5px 24px 24px;
+  padding: 24px;
   display: flex;
   gap: 8px;
   flex-direction: column;
-  min-width: 528px;
+  min-width: 338px;
 
   overflow-y: scroll;
 
   &::-webkit-scrollbar {
-    display: block;
-  }
-
-  &::-webkit-scrollbar {
-    width: 10px;
-  }
-
-  &::-webkit-scrollbar-thumb {
-    height: 10%;
-    background: ${({ theme }) => theme.COLOR['gray-4']};
-    border-radius: 5px;
-  }
-
-  &::-webkit-scrollbar-track {
-    background: ${({ theme }) => theme.COLOR['gray-5']};
-    border-radius: 5px;
+    display: none;
   }
 `;
 
@@ -47,7 +33,7 @@ const ContentPlace = styled.div`
   background: ${({ theme }) => theme.COLOR['gray-6']};
   color: ${({ theme }) => theme.COLOR.white};
   font-family: ${({ theme }) => theme.FONT_FAMILY.Pretendard};
-  font-size: ${({ theme }) => theme.TEXT_SIZE['text-14']};
+  font-size: ${({ theme }) => theme.TEXT_SIZE['text-12']};
   font-weight: ${({ theme }) => theme.FONT_WEIGHT['WEIGHT-400']};
 `;
 
@@ -96,8 +82,8 @@ export const M = {
 
 const OneWrapper = styled.div<{ isSelect: boolean }>`
   cursor: pointer;
-  width: 480px;
-  height: 219px;
+  width: 302px;
+  height: 140px;
   border-radius: 15px;
   background: ${({ theme, isSelect }) =>
     !isSelect ? theme.COLOR['gray-5'] : theme.COLOR['gray-4']};
@@ -106,16 +92,16 @@ const OneWrapper = styled.div<{ isSelect: boolean }>`
       ? `2px solid ${theme.COLOR['gray-5']}`
       : `2px solid ${theme.COLOR['gray-3']}`};
   display: flex;
-  padding: 26px;
-  gap: 24px;
+  padding: 16px;
+  gap: 16px;
 `;
 
 const CardImg = styled.div<{ imgUrl: string }>`
   background: url(${({ imgUrl }) => imgUrl});
   background-size: cover;
   border-radius: 15px;
-  width: 168px;
-  min-height: 168px;
+  width: 107.5px;
+  min-height: 107.5px;
 `;
 
 const CardCreate = styled.div`
@@ -124,56 +110,55 @@ const CardCreate = styled.div`
   font-size: ${({ theme }) => theme.TEXT_SIZE['text-14']};
   font-weight: ${({ theme }) => theme.FONT_WEIGHT['WEIGHT-400']};
   line-height: ${({ theme }) => theme.TEXT_SIZE['text-16']};
-  min-height: 16px;
+  min-height: 10px;
 `;
 
 const CardTitle = styled.div`
-  min-height: 19px;
+  min-height: 12px;
   margin-top: 4px;
   color: ${({ theme }) => theme.COLOR.white};
   font-family: ${({ theme }) => theme.FONT_FAMILY.Pretendard};
-  font-size: ${({ theme }) => theme.TEXT_SIZE['text-16']};
+  font-size: 10px;
   font-weight: ${({ theme }) => theme.FONT_WEIGHT['WEIGHT-700']};
 `;
 
 const CardDescription = styled.div`
-  min-height: 63px;
-  margin-top: 8px;
+  margin-top: 6px;
   white-space: pre-wrap;
   overflow: hidden;
   text-overflow: ellipsis;
-  max-height: 3em;
   color: ${({ theme }) => theme.COLOR.white};
   font-family: ${({ theme }) => theme.FONT_FAMILY.Pretendard};
-  font-size: ${({ theme }) => theme.TEXT_SIZE['text-14']};
+  font-size: 9px;
   font-weight: ${({ theme }) => theme.FONT_WEIGHT['WEIGHT-400']};
   line-height: 140%;
+  ${omitText};
 `;
 
 const CardInfo = styled.div`
-  max-width: 240px;
+  max-width: 154px;
 `;
 
 const CardPlace = styled.div`
   display: flex;
   align-items: center;
-  margin-top: 12px;
+  margin-top: 7px;
   gap: 8px;
   color: ${({ theme }) => theme.COLOR.white};
   font-family: ${({ theme }) => theme.FONT_FAMILY.Pretendard};
-  font-size: ${({ theme }) => theme.TEXT_SIZE['text-14']};
+  font-size: 10px;
   font-weight: ${({ theme }) => theme.FONT_WEIGHT['WEIGHT-400']};
 `;
 
 const TagGroup = styled.div`
-  margin-top: 10px;
+  margin-top: 6px;
   display: flex;
   gap: 4px;
 
   div {
     color: ${({ theme }) => theme.COLOR['gray-6']};
     font-family: ${({ theme }) => theme.FONT_FAMILY.Pretendard};
-    font-size: ${({ theme }) => theme.TEXT_SIZE['text-14']};
+    font-size: 9px;
     font-weight: ${({ theme }) => theme.FONT_WEIGHT['WEIGHT-400']};
   }
 
