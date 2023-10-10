30401d38be6ccb19ff590389b28f5b813eb11a0e
Jihyeong00 <iolpd42@gmail.com>
2023-10-11 00:15:25 +0900

    📝fix : 버그 수정

--- src/components/common/FlipCard/Back/SpaceInfoBack.tsx
+++ src/components/common/FlipCard/Back/SpaceInfoBack.tsx
@@ -3,19 +3,19 @@ import S from '@components/common/FlipCard/Back/style';
 import { SpaceInfoBackPropsType } from '@type/post.type';
 
 const SpaceInfoBack = (props: SpaceInfoBackPropsType) => {
-  const { item, size } = props;
+  const { item, isBig } = props;
 
   const { spaceTitle, userList } = item;
 
   return (
-    <S.Wrapper size={size}>
+    <S.Wrapper isBig={isBig}>
       <S.Shadow />
       <S.Container className={'container'} info="space">
         <S.PostTitle className={'title'}>{spaceTitle}</S.PostTitle>
         <S.InfoBottom className={'info-bottom'} info={'space'}>
           <Avatars
-            max={size === 'big' ? 4 : 5}
-            size={size === 'big' ? 45 : 28}
+            max={isBig ? 4 : 5}
+            size={isBig ? 45 : 28}
             users={userList}
           />
         </S.InfoBottom>
