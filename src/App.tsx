94fc996ba3c70711490c55f677cc87bcc15fa364
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-10-14 22:35:22 +0900

    💄style : 코드 포맷팅 누락

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import router from '@router/Router';
 import KkiriProvider from './utils/KkiriProvider';
 import '@styles/fonts/font.css';
 import { worker } from '@mocks/browser';
-import { motion, useScroll } from 'framer-motion';
 
 function App() {
   worker.start();
