3c588be3913c13f3a8adbd7850dfabee6f1e729e
Jihyeong00 <iolpd42@gmail.com>
2023-10-15 11:12:13 +0900

    🌈feat : useCss, useScript 추가

---
+++ src/hooks/common/useCss.ts
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react';
+
+const useCss = (href: string) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Event | string | Error | null>(null);
+
+  useEffect(() => {
+    let link: HTMLLinkElement = document.querySelector(`link[href="${href}"]`)!;
+
+    if (!link) {
+      link = document.createElement('link');
+      link.href = href;
+      link.rel = 'stylesheet';
+    }
+
+    const handleLoad = () => setLoading(false);
+    const handleError = (error: Event | string | Error) => setError(error);
+
+    link.addEventListener('load', handleLoad);
+    link.addEventListener('error', handleError);
+
+    document.head.appendChild(link);
+
+    return () => {
+      link.removeEventListener('load', handleLoad);
+      link.removeEventListener('error', handleError);
+    };
+  }, [href]);
+
+  return [loading, error];
+};
+
+export default useCss;
