e4f27f9efb420a58c60c018e4491cd0395edf3c3
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-10-16 02:06:17 +0900

    💄style : 코드 포맷팅누락

--- src/router/Private.tsx
+++ src/router/Private.tsx
@@ -1,22 +1,22 @@
-import {Outlet, useNavigate} from 'react-router-dom';
-import {PATH} from '@constants/path';
-import {useEffect} from 'react';
-import {useUserStore} from '@store/user';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { PATH } from '@constants/path';
+import { useEffect } from 'react';
+import { useUserStore } from '@store/user';
 
 type PrivateRouteType = {
-    isAllowed: boolean;
+  isAllowed: boolean;
 };
 
-const PrivateRoute = ({isAllowed}: PrivateRouteType) => {
-    const navigate = useNavigate();
-    const isLogin = useUserStore((e) => e.isLogin);
-    useEffect(() => {
-        if (!isAllowed && !isLogin) {
-            navigate(PATH.LOGIN);
-        }
-    }, [isAllowed, isLogin, navigate]);
+const PrivateRoute = ({ isAllowed }: PrivateRouteType) => {
+  const navigate = useNavigate();
+  const isLogin = useUserStore((e) => e.isLogin);
+  useEffect(() => {
+    if (!isAllowed && !isLogin) {
+      navigate(PATH.LOGIN);
+    }
+  }, [isAllowed, isLogin, navigate]);
 
-    return <Outlet/>;
+  return <Outlet />;
 };
 
 export default PrivateRoute;
