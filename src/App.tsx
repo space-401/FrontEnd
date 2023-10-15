5d3ce0e4dd41817f54d66963ea18309797578d4a
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-10-15 19:16:52 +0900

    🌈feat : 쿠키, 토큰스토리지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import { RouterProvider } from 'react-router-dom';
 import router from '@router/Router';
-import KkiriProvider from './utils/KkiriProvider';
 import '@styles/fonts/font.css';
 import { worker } from '@mocks/browser';
+import { queryClient } from '@hooks/api/queryClient';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import GlobalStyles from '@styles/global';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '@styles/theme/theme';
+import { QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
   worker.start();
   return (
-    <KkiriProvider>
-      <RouterProvider router={router} />
-    </KkiriProvider>
+    <QueryClientProvider client={queryClient}>
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+        <GlobalStyles />
+        <ThemeProvider theme={theme}>
+          <RouterProvider router={router} />
+        </ThemeProvider>
+      </GoogleOAuthProvider>
+    </QueryClientProvider>
   );
 }
 
