d421aeac0bdbc048880a6bb2028044768a5592c5
ooherin <555ohr@naver.com>
2023-11-01 09:41:43 +0900

    fix: env파일 수정

--- src/components/Auth/SocialNaver.tsx
+++ src/components/Auth/SocialNaver.tsx
@@ -1,9 +1,9 @@
-import { NAVER_LOGIN_URL, LOGIN_REDIRECT_URI } from '@/constants/auth';
+import { LOGIN_REDIRECT_URI, NAVER_LOGIN_URL } from '@/constants/auth';
 
 export const SocialNaver = () => {
   const naverURL = `${NAVER_LOGIN_URL}?client_id=${
     import.meta.env.VITE_NAVER_CLIENT_ID
-  }&redirect_uri=${LOGIN_REDIRECT_URI}/naver&response_type=code`;
+  }&redirect_uri=${LOGIN_REDIRECT_URI}&response_type=code&state=false`;
   const naverHandleLogin = () => {
     window.location.href = naverURL;
   };
--- src/pages/Auth/index.tsx
+++ src/pages/Auth/index.tsx
@@ -1,15 +1,28 @@
-import { useLogInMutation } from '@hooks/api/user/useLogInMutation';
 import Loading from '@pages/Loading';
 import { useEffect } from 'react';
+import axios from 'axios';
 
 const Auth = () => {
-  const code = new URL(window.location.href).searchParams.get('code');
-  const { loginAction } = useLogInMutation();
   useEffect(() => {
-    loginAction(code!);
-  }, [code, loginAction]);
+    const code = new URL(window.location.href).searchParams.get('code');
+    const state = new URL(window.location.href).searchParams.get('state');
+    console.log('code', code);
+    console.log('state', state);
+    const fetchData = async () => {
+      if (code && state) {
+        try {
+          const response = await axios.get(
+            `http://52.79.57.246:8081/user/auth/kakao?code=${code}&state=${state}`
+          );
+          console.log('data', response.data);
+        } catch (error) {
+          console.error('Error:', error);
+        }
+      }
+    };
+    fetchData();
+  }, []);
 
   return <Loading />;
 };
-
 export default Auth;
