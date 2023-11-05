6c16a78c0de99a38c1dd3a6c42b3dd94015232a7
ooherin <555ohr@naver.com>
2023-11-05 10:36:48 +0900

    🌈feat : 로그인 api 진행중

--- src/apis/AxiosInstance.ts
+++ src/apis/AxiosInstance.ts
@@ -6,54 +6,47 @@ export const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_BACK_URL,
   timeout: NETWORK.TIMEOUT,
   withCredentials: true,
-  useAuth: true,
   headers: {
     Authorization: `Bearer ${tokenStorage.getAccessToken()}`,
   },
 });
-// axiosInstance.interceptors.request.use(checkAndSetToken, handleAPIError);
 
+let isRefreshing = false;
 axiosInstance.interceptors.response.use(
   //성공시 발생
   (response) => response,
   async (error) => {
     if (error.response?.status === 401) {
-      console.log('401');
-      //리프레시 토큰도 없는 경우 로그인으로 리다이렉트
+      tokenStorage.removeAccessToken();
       if (!tokenStorage.hasRefreshToken()) {
+        //리프레시 토큰도 없는 경우 로그인으로 리다이렉트
         window.location.href = '/login';
       } else {
-        await tokenRefresh();
-        //재발급한 토큰으로 재요청
-        return axiosInstance(error.config);
+        //리프레싱이 false 일 떄만 토큰 재발급 시도
+        if (!isRefreshing) {
+          isRefreshing = true;
+          try {
+            const { data } = await axios.get(
+              `${import.meta.env.VITE_BACK_URL}/user/refreshToken`,
+              {
+                headers: {
+                  Authorization: `Bearer ${tokenStorage.getRefreshToken()}`,
+                },
+              }
+            );
+            tokenStorage.setAccessToken(data.newAccessToken, 30);
+          } catch (error) {
+            throw new Error('토큰 요청 실패');
+          }
+          //재발급한 토큰으로 재요청
+          console.log('재요청 토큰', tokenStorage.getAccessToken());
+          await axiosInstance(error.config);
+        }
       }
     }
   }
 );
-//
-let isRefreshing = false;
-const tokenRefresh = async () => {
-  if (!isRefreshing) {
-    isRefreshing = true;
-    try {
-      const { data } = await axiosInstance.get(
-        `${import.meta.env.VITE_BACK_URL} + /user/refreshToken`,
-        {
-          headers: {
-            'Content-Type': 'application/json',
-            'RefreshToken': `Bearer ${tokenStorage.getRefreshToken()}`,
-          },
-        }
-      );
-      tokenStorage.setAccessToken(data.accessToken, 30);
-    } catch (error) {
-      // 에러 처리
-      console.log('토큰 요청 실패');
-    } finally {
-      isRefreshing = false;
-    }
-  }
-};
+
 //에러시 발생
 // handleTokenError
 
--- src/apis/user/getLogin.ts
+++ src/apis/user/getLogin.ts
@@ -4,20 +4,13 @@ import { SocialType } from '@type/user.type';
 export const getLogin = async ({
   code,
   socialType,
-  scope,
 }: {
   code: string;
   socialType: SocialType;
-  scope?: string;
 }) => {
-  let socialUrl;
-  if (socialType === 'kakao') {
-    socialUrl = `/user/auth/${socialType}?code=${code}&state=false`;
-  } else if (socialType === 'naver') {
-    socialUrl = `/user/auth/${socialType}?code=${code}&state=false`;
-  } else {
-    socialUrl = `/user/auth/${socialType}?code=${code}&scope=${scope}`;
-  }
+  const socialUrl = `/user/auth/${socialType}?code=${code}&state=false`;
+  console.log('socialUrl', socialUrl);
+
   const response = await axios.get(import.meta.env.VITE_BACK_URL + socialUrl);
   return response.data;
 };
