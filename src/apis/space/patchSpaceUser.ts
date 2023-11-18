0bcf572627ceb4cc2c85292dbc04ee5136215e74
ooherin <555ohr@naver.com>
2023-11-18 13:35:07 +0900

    📝fix : 방장 바꾸는 로직 수정

--- src/apis/space/patchSpaceUser.ts
+++ src/apis/space/patchSpaceUser.ts
@@ -1,18 +1,18 @@
-import { axiosInstance } from '@apis/AxiosInstance';
-import { END_POINTS } from '@constants/api';
-import { ApiResponseType } from '@/types/response.type';
+import {axiosInstance} from '@apis/AxiosInstance';
+import {END_POINTS} from '@constants/api';
+import {ApiResponseType} from '@/types/response.type';
 
-export type SpaceUserPropsType = {
-  spaceId: number;
-  userId?: number; //해당 유저가 방장이 되는 경우에만 추가함.
-  isAdmin?: boolean;
-  image?: File | null;
-  userNickName?: string;
-};
-export const patchSpaceUser = async (patchInfo: SpaceUserPropsType) => {
+// export type SpaceUserPropsType = {
+//   spaceId: number;
+//   userId?: number; //해당 유저가 방장이 되는 경우에만 추가함.
+//   isAdmin?: boolean;
+//   image?: File | null;
+//   userNickName?: string;
+// };
+export const patchSpaceUser = async (patchInfo: FormData) => {
   const { data } = await axiosInstance.patch<ApiResponseType>(
     END_POINTS.SPACE_USER,
-    { ...patchInfo },
+    patchInfo,
     {
       headers: {
         'Content-Type': 'multipart/form-data',
--- src/hooks/api/space/useSpaceUserUpdateMutation.ts
+++ src/hooks/api/space/useSpaceUserUpdateMutation.ts
@@ -2,15 +2,14 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import type { AxiosError } from 'axios';
 import { patchSpaceUser } from '@/apis/space/patchSpaceUser';
 import { ApiResponseType } from '@/types/response.type';
-import { SpaceUserPropsType } from '@/apis/space/patchSpaceUser';
 
 export const useSpaceUserUpdateMutation = (spaceId: string) => {
   const queryClient = useQueryClient();
   const { mutate: userUpdateAction } = useMutation<
     ApiResponseType,
     AxiosError,
-    SpaceUserPropsType
-  >((userInfo) => patchSpaceUser(userInfo), {
+    FormData
+  >((formData: FormData) => patchSpaceUser(formData), {
     onSuccess: async () => {
       await queryClient.invalidateQueries(['spaceInfo', Number(spaceId)]);
     },
