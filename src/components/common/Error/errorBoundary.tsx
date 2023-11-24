17ef20ba5e7929b433640205572297183296a8f3
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2023-11-25 01:32:33 +0900

    🚧refactor : 에러바운더리 추가 진행중

--- src/components/common/Error/errorBoundary.tsx
+++ src/components/common/Error/errorBoundary.tsx
@@ -1,66 +1,56 @@
-import React, { ReactNode } from 'react';
+import { Component } from 'react';
+import { HTTPError } from '@apis/HTTPError';
+import ErrorPage from '@components/common/Error/index';
+import { Outlet } from 'react-router-dom';
 
 interface Props {
-  children?: ReactNode;
-  fallback: React.ElementType;
   message?: string;
-  onReset?: () => void;
+  onReset?: (error: Error | HTTPError) => void;
   isRefresh?: boolean;
 }
 
 interface State {
   hasError: boolean;
-  info: Error | null;
+  error: Error | null;
 }
 
 const initialState: State = {
   hasError: false,
-  info: null,
+  error: null,
 };
 
-class ErrorBoundary extends React.Component<Props, State> {
+class ErrorBoundary extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = initialState;
+    this.resetErrorBoundary = this.resetErrorBoundary.bind(this);
   }
 
-  static getDerivedStateFromError(error: Error) {
-    return { hasError: true, info: error };
-  }
-
-  onResetErrorBoundary = () => {
+  resetErrorBoundary = () => {
     const { onReset } = this.props;
-    onReset == null ? void 0 : onReset();
-    //onReset이 있다면 호출하고 아니면 호출하지 않는다.
-    this.reset();
+    const { error } = this.state;
+
+    onReset?.(error!);
+    this.setState(initialState);
   };
 
-  reset() {
-    this.setState(initialState);
+  static getDerivedStateFromError(error: Error) {
+    return { hasError: true, error };
   }
 
   render() {
-    const { hasError, info } = this.state;
-    const { children, message, isRefresh } = this.props;
-
-    console.log('에러바운더리', info);
-    //인포의 분기점에 따라 다른 에러 바운더리 실행
-    //메세지와 버튼을 다르게 지정하기
-    if (hasError) {
-      const props = {
-        error: info,
-        onResetErrorBoundary: this.onResetErrorBoundary,
-      };
+    const { error } = this.state;
+    if (error) {
       return (
-        <this.props.fallback
-          isRefresh={isRefresh}
-          onRefresh={this.reset}
-          onReset={props.onResetErrorBoundary}
-          message={message}
+        <ErrorPage
+          statusCode={error instanceof HTTPError ? error.statusCode : undefined}
+          errorCode={error instanceof HTTPError ? error.code : undefined}
+          resetError={this.resetErrorBoundary}
         />
       );
     }
-    return children;
+
+    return <Outlet />;
   }
 }
 
