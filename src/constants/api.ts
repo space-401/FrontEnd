b5e54673e540244c6db3a8d9429c7677f9b3394c
Jihyeong00 <iolpd42@gmail.com>
2023-11-25 15:06:10 +0900

    fix : msw를 비활성화 합니다.

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,11 +1,9 @@
 export const Product = process.env.NODE_ENV === 'production';
-export const Dev = process.env.NODE_ENV !== 'development';
+export const Dev = process.env.NODE_ENV === 'development';
 export const NETWORK = {
   RETRY_COUNT: 2,
   TIMEOUT: 10000,
 };
-
-// export const AXIOS_BASE_URL = Product ? import.meta.env.VITE_BACK_URL : '/';
 export const AXIOS_BASE_URL = import.meta.env.VITE_BACK_URL;
 export const END_POINTS = {
   USER: '/user',
