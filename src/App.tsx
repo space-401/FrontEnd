95e88618f19f646d340db159022865117d7532ef
ooherin <555ohr@naver.com>
2023-12-01 01:57:11 +0900

    🌈feat : 허스키 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import { RouterProvider } from 'react-router-dom';
 import router from '@router/Router';
 import KkiriProvider from './utils/KkiriProvider';
 import '@styles/fonts/font.css';
-import { worker } from '@mocks/browser';
+// import { worker } from '@mocks/browser';
 import { motion, useScroll } from 'framer-motion';
 
 function App() {
   const { scrollYProgress } = useScroll();
-  worker.start();
+  //worker.start();
   return (
     <KkiriProvider>
       <motion.div
