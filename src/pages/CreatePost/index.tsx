d7d2ebd8571d0c40ebe550d916daf4e5710027b8
ooherin <555ohr@naver.com>
2023-12-05 00:43:29 +0900

    🌈feat : 게시물 수정시 이미지 수정 로직 추가

--- src/pages/CreatePost/index.tsx
+++ src/pages/CreatePost/index.tsx
@@ -162,6 +162,13 @@ const CreatePost = () => {
 
   const confirmOpen = useConfirmModalOpen();
 
+  const resetImg = () => {
+    setImageArr({
+      images: [],
+      cropImages: [],
+      convertedImages: [],
+    });
+  };
   const confirmModalOpen = () => {
     confirmOpen({
       AsyncAction: () => {},
@@ -171,6 +178,16 @@ const CreatePost = () => {
       titleMessage: postId
         ? '성공적으로 수정되었습니다. '
         : '성공적으로 포스팅되었습니다.',
+    });
+  };
+
+  const resetConfirmModalOpen = () => {
+    confirmOpen({
+      AsyncAction: resetImg,
+      isPositiveModal: true,
+      ApproveMessage: '확인',
+      closeMessage: '닫기',
+      titleMessage: '기존의 사진이 삭제됩니다. 다시 사진을 선택하시겠습니까?',
     });
   };
 
@@ -303,6 +320,15 @@ const CreatePost = () => {
               </BasicButton>
             </S.PhotoContainer>
           </BasicBox>
+        </S.BoxWrapper>
+      ) : postId ? (
+        <S.BoxWrapper>
+          <div style={{ zIndex: 1000, width: '348px' }}>
+            <ImgSlider
+              onClickImgEditModal={resetConfirmModalOpen}
+              images={imageArr.cropImages}
+            />
+          </div>
         </S.BoxWrapper>
       ) : (
         <S.BoxWrapper>
